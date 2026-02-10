33be6f5130e962b7e5d6c3fd00361107356752c8
Flora Moon <fmoon@getplume.co>
2026-02-10 15:12:08 -0800

    replace eslint-lsp with eslint_d

--- init.lua
+++ init.lua
@@ -638,7 +638,7 @@ require('lazy').setup({
         'stylua', -- Used to format Lua code
         -- You can add other tools here that you want Mason to install
         'prettierd',
-        'eslint-lsp',
+        'eslint_d',
       })
 
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
@@ -895,7 +895,7 @@ require('lazy').setup({
   --
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
