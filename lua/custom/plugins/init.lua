24ada37f82d2fea398ad5b6bdc8c84c22c149cd7
Flora Moon <fmoon@getplume.co>
2026-02-11 13:56:10 -0800

    add nexttab,previoustab key shortcuts

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,8 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+vim.keymap.set('n', 'H', ':tabprevious<CR>', { desc = 'Previous Tab' })
+vim.keymap.set('n', 'L', ':tabnext<CR>', { desc = 'Next Tab' })
+
 return {}
