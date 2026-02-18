0971e46ab9429aca5ffe2a16364bb15148411448
Flora Moon <fmoon@getplume.co>
2026-02-18 14:07:18 -0800

    add tablose, tabsplit hotkeys

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,7 +3,9 @@
 --
 -- See the kickstart.nvim README for more information
 
+vim.keymap.set('n', '<leader>bd', ':tabclose<CR>', { desc = 'Next Tab' })
 vim.keymap.set('n', 'H', ':tabprevious<CR>', { desc = 'Previous Tab' })
 vim.keymap.set('n', 'L', ':tabnext<CR>', { desc = 'Next Tab' })
+vim.keymap.set('n', 'T', ':tab split<CR>', { desc = 'Next Tab' })
 
 return {}
